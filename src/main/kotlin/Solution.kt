7303096fbb69c6cf910030e2bee80fbe0e2c463d
gson Liang <yuanyu90221@gmail.com>
2022-03-09 19:39:54 +0800

    [fix] add edge case test

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -4,16 +4,25 @@ class Solution {
         var max = 0
         var idx = 0
         var accum = 0
+        var preChar: Char = Char.MIN_VALUE
         for (ch in input) {
             idx++
             if (charMap.containsKey(ch)) {
                 if (max <= accum) {
                     max = accum
                 }
+                if (preChar == ch) {
+                    accum = 1
+                    charMap.clear()
+                }
             } else {
                 accum++
             }
             charMap[ch] = idx
+            preChar = ch
+        }
+        if (max <= accum) {
+            max = accum
         }
         return max
     }
--- src/test/kotlin/SolutionTest.kt
+++ src/test/kotlin/SolutionTest.kt
@@ -7,4 +7,8 @@ internal class SolutionTest {
     fun findMaxDistinctExample1() {
         assertEquals(5, sol.findMaxDistinct("jiujitsu"))
     }
+    @Test
+    fun findMaxDistinctExample2() {
+        assertEquals(4, sol.findMaxDistinct("jiujjiitsu"))
+    }
 }
